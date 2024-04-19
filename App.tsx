976f241972c7f8a0da9f1d2011e77d7bdb804eaa
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-19 22:12:35 +0400

    Three pages of Stack.screen

--- App.tsx
+++ App.tsx
@@ -1,26 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
+function HomeScreen({navigation}:any) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={{marginBottom:30}}>Home Screen</Text>
+            <Button title={"go to profile"} onPress={()=>navigation.navigate("profile")}/>
+            <Button title={"go to user"} onPress={()=>navigation.navigate("user")}/>
+        </View>
+    );
+}
+
+function ProfileScreen({navigation}:any) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{marginBottom:30}}>Profile Screen</Text>
+            <Button title={"go to home"} onPress={()=>navigation.navigate("home")}/>
+            <Button title={"go to user"} onPress={()=>navigation.navigate("user")}/>
+
+        </View>
+    );
+}
+
+function UserScreen({navigation}:any) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{marginBottom:30}}>User Screen</Text>
+            <Button title={"go to home"} onPress={()=>navigation.navigate("home")}/>
+            <Button title={"go to profile"} onPress={()=>navigation.navigate("profile")}/>
+
+        </View>
+    );
+}
 
 export default function App() {
-  return (
-
-    <View style={styles.container}>
-
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={"home"} component={HomeScreen}/>
+                <Stack.Screen name={"profile"} component={ProfileScreen}/>
+                <Stack.Screen name={"user"} component={UserScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
